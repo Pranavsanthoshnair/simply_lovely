9d666ca0b0ffcc81917a397f0df76d2542ed79a4
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-01 09:28:59 +0530

    Refactor sparse matrix operations and main logic

--- 02_sum_of_sparse_matrices_and_its_transpose.c
+++ 02_sum_of_sparse_matrices_and_its_transpose.c
@@ -1,153 +1,119 @@
 #include <stdio.h>
 
-typedef struct {
-    int row, col, val;
-} Element;
+int main() {
+    int m1[10][10], m2[10][10];
+    int sm1[100][3], sm2[100][3], sum[100][3];
+    int r, c, n1 = 0, n2 = 0;
 
-int size;
-int A[10][10], B[10][10];
-Element cA[100], cB[100], cSum[200], cTrans[200];
+    printf("Enter rows and cols: ");
+    scanf("%d%d", &r, &c);
 
-void inputMatrix(int M[10][10]) {
-    int n, r, c, v;
-    printf("Enter number of non-zero elements: ");
-    scanf("%d", &n);
-    
-    for (int i = 0; i < n; i++) {
-        printf("Enter row col value: ");
-        scanf("%d %d %d", &r, &c, &v);
-        M[r][c] = v;
-    }
-}
+    printf("Enter elements of Matrix 1:\n");
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &m1[i][j]);
+            if(m1[i][j] != 0) n1++;
+        }
 
-int toCompact(int M[10][10], Element C[100]) {
+    printf("Enter elements of Matrix 2:\n");
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &m2[i][j]);
+            if(m2[i][j] != 0) n2++;
+        }
+
+    sm1[0][0] = r; sm1[0][1] = c; sm1[0][2] = n1;
     int k = 1;
-    C[0].row = size;
-    C[0].col = size;
-    
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            if (M[i][j] != 0) {
-                C[k].row = i;
-                C[k].col = j;
-                C[k].val = M[i][j];
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            if(m1[i][j] != 0) {
+                sm1[k][0] = i;
+                sm1[k][1] = j;
+                sm1[k][2] = m1[i][j];
                 k++;
             }
-    
-    C[0].val = k - 1;
-    return k - 1;
-}
 
-int addCompact(Element A[100], Element B[100], Element R[200]) {
-    int i = 1, j = 1, k = 1;
-    R[0].row = size;
-    R[0].col = size;
-    
-    // Merge sorted lists
-    while (i <= A[0].val && j <= B[0].val) {
-        if (A[i].row < B[j].row || (A[i].row == B[j].row && A[i].col < B[j].col)) {
-            R[k++] = A[i++];
+    sm2[0][0] = r; sm2[0][1] = c; sm2[0][2] = n2;
+    int p = 1;
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            if(m2[i][j] != 0) {
+                sm2[p][0] = i;
+                sm2[p][1] = j;
+                sm2[p][2] = m2[i][j];
+                p++;
+            }
+
+    printf("\nSparse of Matrix 1:\n");
+    for(int i = 0; i <= n1; i++)
+        printf("%d\t%d\t%d\n", sm1[i][0], sm1[i][1], sm1[i][2]);
+
+    printf("\nSparse of Matrix 2:\n");
+    for(int i = 0; i <= n2; i++)
+        printf("%d\t%d\t%d\n", sm2[i][0], sm2[i][1], sm2[i][2]);
+
+    printf("\nSum Sparse Matrix:\n");
+    int i = 1, j = 1, m = 1;
+    sum[0][0] = r;
+    sum[0][1] = c;
+
+    while(i <= n1 && j <= n2) {
+        if(sm1[i][0] < sm2[j][0] || (sm1[i][0] == sm2[j][0] && sm1[i][1] < sm2[j][1])) {
+            sum[m][0] = sm1[i][0];
+            sum[m][1] = sm1[i][1];
+            sum[m][2] = sm1[i][2];
+            i++; m++;
         }
-        else if (B[j].row < A[i].row || (B[j].row == A[i].row && B[j].col < A[i].col)) {
-            R[k++] = B[j++];
+        else if(sm1[i][0] > sm2[j][0] || (sm1[i][0] == sm2[j][0] && sm1[i][1] > sm2[j][1])) {
+            sum[m][0] = sm2[j][0];
+            sum[m][1] = sm2[j][1];
+            sum[m][2] = sm2[j][2];
+            j++; m++;
         }
         else {
-            R[k].row = A[i].row;
-            R[k].col = A[i].col;
-            R[k].val = A[i].val + B[j].val;
-            k++; i++; j++;
+            sum[m][0] = sm1[i][0];
+            sum[m][1] = sm1[i][1];
+            sum[m][2] = sm1[i][2] + sm2[j][2];
+            i++; j++; m++;
         }
     }
-    
-    while (i <= A[0].val) R[k++] = A[i++];
-    while (j <= B[0].val) R[k++] = B[j++];
-    
-    R[0].val = k - 1;
-    return k - 1;
-}
 
-void toMatrix(Element C[200], int M[10][10]) {
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            M[i][j] = 0;
-    
-    for (int i = 1; i <= C[0].val; i++)
-        M[C[i].row][C[i].col] = C[i].val;
-}
+    while(i <= n1) {
+        sum[m][0] = sm1[i][0];
+        sum[m][1] = sm1[i][1];
+        sum[m][2] = sm1[i][2];
+        i++; m++;
+    }
+    while(j <= n2) {
+        sum[m][0] = sm2[j][0];
+        sum[m][1] = sm2[j][1];
+        sum[m][2] = sm2[j][2];
+        j++; m++;
+    }
 
-void transpose(Element S[200], Element T[200]) {
-    T[0].row = S[0].col;
-    T[0].col = S[0].row;
-    T[0].val = S[0].val;
-    int k = 1;
-    
-    for (int i = 0; i < size; i++)
-        for (int j = 1; j <= S[0].val; j++)
-            if (S[j].col == i) {
-                T[k].row = S[j].col;
-                T[k].col = S[j].row;
-                T[k].val = S[j].val;
-                k++;
+    sum[0][2] = m - 1;
+
+    for(int x = 0; x < m; x++)
+        printf("%d\t%d\t%d\n", sum[x][0], sum[x][1], sum[x][2]);
+
+    printf("\nTranspose of Sum (Sparse):\n");
+    int tr[100][3];
+    tr[0][0] = sum[0][1];
+    tr[0][1] = sum[0][0];
+    tr[0][2] = sum[0][2];
+
+    int t = 1;
+    for(int col = 0; col < c; col++)
+        for(int x = 1; x < m; x++)
+            if(sum[x][1] == col) {
+                tr[t][0] = sum[x][1];
+                tr[t][1] = sum[x][0];
+                tr[t][2] = sum[x][2];
+                t++;
             }
-}
 
-void showMatrix(int M[10][10]) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            printf("%3d ", M[i][j]);
-        printf("\n");
-    }
-}
+    for(int x = 0; x < t; x++)
+        printf("%d\t%d\t%d\n", tr[x][0], tr[x][1], tr[x][2]);
 
-void showCompact(Element C[200]) {
-    printf("Row Col Val\n");
-    for (int i = 0; i <= C[0].val; i++)
-        printf("%3d %3d %3d\n", C[i].row, C[i].col, C[i].val);
-}
-
-int main() {
-    printf("Enter matrix size: ");
-    scanf("%d", &size);
-    
-    printf("\nMatrix A:\n");
-    inputMatrix(A);
-    printf("\nMatrix B:\n");
-    inputMatrix(B);
-    
-    toCompact(A, cA);
-    toCompact(B, cB);
-    
-    int choice;
-    while (1) {
-        printf("\n1.Add 2.Transpose 3.Show A 4.Show B 5.Exit\n");
-        scanf("%d", &choice);
-        
-        if (choice == 1) {
-            addCompact(cA, cB, cSum);
-            int sum[10][10];
-            toMatrix(cSum, sum);
-            printf("\nSum:\n");
-            showMatrix(sum);
-        }
-        else if (choice == 2) {
-            if (cSum[0].val > 0) {
-                transpose(cSum, cTrans);
-                printf("\nTranspose:\n");
-                showCompact(cTrans);
-            }
-        }
-        else if (choice == 3) {
-            printf("\nMatrix A:\n");
-            showMatrix(A);
-            showCompact(cA);
-        }
-        else if (choice == 4) {
-            printf("\nMatrix B:\n");
-            showMatrix(B);
-            showCompact(cB);
-        }
-        else if (choice == 5) break;
-    }
-    
     return 0;
 }
