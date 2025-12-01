a60a8e1b88b8d6a44c59684f1ef0b866bed5b365
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-01 09:09:13 +0530

    Refactor polynomial addition and display logic

--- 01_sum_of_polynomials_using_array.c
+++ 01_sum_of_polynomials_using_array.c
@@ -1,72 +1,65 @@
 #include <stdio.h>
-
-struct Term {
-    int coeff, expo;
+struct Term{
+    int coeff,exp;
 };
 
-void addPoly(struct Term p1[], int n1, struct Term p2[], int n2, struct Term res[], int *nRes) {
-    int i = 0, j = 0, k = 0;
-    
-    // Compare and merge
-    while (i < n1 && j < n2) {
-        if (p1[i].expo == p2[j].expo) {
+int main(){
+    int n1,n2,nres;
+    printf("Enter number of terms in poly 1 : ");
+    scanf("%d",&n1);
+    printf("Enter number of terms in poly 2 : ");
+    scanf("%d",&n2);
+
+    struct Term p1[n1],p2[n2],res[40];
+
+    printf("Enter terms of Poly1 : \n");
+    for(int i = 0 ; i < n1 ; i++){
+        scanf("%d %d",&p1[i].coeff,&p1[i].exp);
+    }
+
+    printf("Enter terms of Poly 2 : \n");
+    for(int j = 0 ; j < n2 ; j++){
+        scanf("%d %d",&p2[j].coeff,&p2[j].exp);
+    }
+
+    int i = 0 , j = 0 , k = 0;
+    while (i<n1 && j<n2){
+        if(p1[i].exp == p2[j].exp){
             res[k].coeff = p1[i].coeff + p2[j].coeff;
-            res[k].expo = p1[i].expo;
+            res[k].exp = p1[i].exp;
             i++; j++; k++;
         }
-        else if (p1[i].expo > p2[j].expo) {
+        else if (p1[i].exp > p2[j].exp){
             res[k] = p1[i];
             i++; k++;
         }
-        else {
+        else{
             res[k] = p2[j];
             j++; k++;
         }
     }
-    
-    while (i < n1) res[k++] = p1[i++];
-    while (j < n2) res[k++] = p2[j++];
-    
-    *nRes = k;
-}
 
-void display(struct Term p[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%dx^%d", p[i].coeff, p[i].expo);
-        if (i != n - 1) printf(" + ");
+    while(i<n1) res[k++] = p1[i++];
+    while(j<n2) res[k++] = p2[j++];
+
+    nres = k;
+
+    printf("\n1st poly: ");
+    for(int i = 0 ; i < n1 ; i++){
+        printf("%dx^%d",p1[i].coeff,p1[i].exp);
+        if(i!=n1-1) printf(" + ");
+    }
+
+    printf("\n2nd poly: ");
+    for(int j = 0 ; j < n2 ; j++){
+        printf("%dx^%d",p2[j].coeff,p2[j].exp);
+        if(j!=n2-1) printf(" + ");
+    }
+
+    printf("\nResult: ");
+    for(int k = 0 ; k < nres ; k++){
+        printf("%dx^%d",res[k].coeff,res[k].exp);
+        if(k!=nres-1) printf(" + ");
     }
     printf("\n");
-}
-
-void read(struct Term p[], int *n) {
-    printf("Enter number of terms: ");
-    scanf("%d", n);
-    for (int i = 0; i < *n; i++) {
-        printf("Term %d (coeff expo): ", i + 1);
-        scanf("%d %d", &p[i].coeff, &p[i].expo);
-    }
-}
-
-int main() {
-    struct Term p1[20], p2[20], res[40];
-    int n1, n2, nRes;
-    
-    printf("First Polynomial:\n");
-    read(p1, &n1);
-    
-    printf("\nSecond Polynomial:\n");
-    read(p2, &n2);
-    
-    printf("\nP1: ");
-    display(p1, n1);
-    
-    printf("P2: ");
-    display(p2, n2);
-    
-    addPoly(p1, n1, p2, n2, res, &nRes);
-    
-    printf("Sum: ");
-    display(res, nRes);
-    
-    return 0;
 }
